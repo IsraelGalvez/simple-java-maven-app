2301e0a6e02da99688387a87255e74e77979df04
BuhM0 <35952736+BuhMo@users.noreply.github.com>
2022-04-20 15:07:41 -0700

    AAAA

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -7,6 +7,7 @@ job('Java Maven App DSL 3') {
         }
     }
     triggers {
+	cron('H/1 * * * *')
     	githubPush()
     }    
     steps {
@@ -49,7 +50,7 @@ job('Java Maven App DSL 3') {
 job('Job test Hola Mundo') {
 	description('Aplicacion Hola Mundo de Prueba')
 	triggers {
-		cron('H/1 * * * *')
+		cron('H/7 * * * *')
     		githubPush()
    	}
 	steps {
